9ed5604356484e0fb75f418a9c4142595fb17d8c
Stephan Jess <s.jess@novagraphix.de>
2014-07-16 09:31:21 +0200

    initial commit

---
+++ nova.ytOverlay.js
@@ -0,0 +1,90 @@
+/**
+ * ytOverlay jQuery Plugin
+ *
+ */
+;( function( $, window ) {
+
+  $.fn.ytOverlay = function( options ) {
+
+    var $this = $(this),
+        docElem = window.document.documentElement,
+        win = { width : getViewport('x'), height : getViewport('y') };
+
+    // Settings
+    var settings = $.extend({
+        youtubeID : '',
+        trigger : 'trailer',
+        top : '100',
+        right : '100',
+        bottom : '100',
+        left : '100',
+        overlayAlpha : '0.7',
+        overlayColor : '0,0,0'
+    }, options );
+
+    var url = '//www.youtube.com/embed/'+settings.youtubeID+'?rel=0',
+        url_autoplay = '//www.youtube.com/embed/'+settings.youtubeID+'?rel=0&autoplay=1';
+
+    function init() {
+      $('body').append('<div id="overlay"><iframe id="ytvideo" src="'+url+'" frameborder="0" allowfullscreen></iframe><div id="close"><img src="images/cross.png" alt=""></div>');
+      var overlay_styles = {
+        "display" : "none",
+        "position" : "fixed",
+        "top" : "0",
+        "left" : "0",
+        "width" : "100%",
+        "height" : "100%",
+        "background-color" : "rgba("+settings.overlayColor+","+settings.overlayAlpha+")",
+        "z-index" : "1000"
+      }
+      $('#overlay').css(overlay_styles);
+      var close_styles = {
+        "cursor" : "pointer",
+        "position" : "absolute",
+        "top" : "15px",
+        "right" : "15px"
+      }
+      $('#close').css(close_styles);
+    }
+
+    init();
+
+    /* Window Resize Events */
+    $(window).resize(function() {
+        win.width = getViewport('x');
+        win.height = getViewport('y');
+        $('#ytvideo').height(win.height-settings.top-settings.bottom).width(win.width-settings.right-settings.left).css({'marginLeft' : settings.left+'px', 'marginTop' : settings.top+'px', 'marginRight' : settings.right+'px', 'marginBottom' : settings.bottom+'px'});
+    }).trigger('resize');
+
+    function getViewport(axis) {
+        var client, inner;
+        if( axis === 'x' ) {
+            client = docElem['clientWidth'];
+            inner = window['innerWidth'];
+        }
+        else if( axis === 'y' ) {
+            client = docElem['clientHeight'];
+            inner = window['innerHeight'];
+        }
+
+        return client < inner ? inner : client;
+    }
+
+    $('#'+settings.trigger).click(function(event) {
+        event.preventDefault();
+        $('#overlay').fadeIn();
+        $('#ytvideo').attr({
+            src: url_autoplay
+        });
+    });
+
+    $('#close').click(function(event) {
+        event.preventDefault();
+        $('#overlay').fadeOut();
+        $('#ytvideo').attr({
+            src: url
+        });
+    });
+
+  };
+}( jQuery, window, undefined ));
